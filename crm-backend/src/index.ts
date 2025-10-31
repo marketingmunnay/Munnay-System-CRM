a4c8471441cac77da54ea57dd9cd1c46e2c4e2d4
Marketing munnay <marketingmunnay@gmail.com>
2025-10-31 11:11:47 -0500

    feat: agregar endpoint de login y migración default avatar

--- crm-backend/src/index.ts
+++ crm-backend/src/index.ts
@@ -56,5 +56,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
