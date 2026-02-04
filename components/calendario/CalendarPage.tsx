6a90b8ef26ea8e6fc078fa0323111170a7084cce
Marketing munnay <marketingmunnay@gmail.com>
2026-02-04 16:54:40 -0500

    fix: mover animaciones shake y fade-in a index.css y eliminar estilos inline del calendario

--- components/calendario/CalendarPage.tsx
+++ components/calendario/CalendarPage.tsx
@@ -947,6 +947,12 @@ const CalendarPage: React.FC<CalendarPageProps> = ({
             {toast}
         </div>
     )}
+        {/* Toast visual de error */}
+        {toast && (
+            <div className="fixed bottom-8 left-1/2 transform -translate-x-1/2 bg-red-600 text-white px-6 py-3 rounded-xl shadow-lg z-50 animate-fade-in">
+                {toast}
+            </div>
+        )}
 /* Animación shake y fade-in para feedback visual */
 <style jsx global>{`
 @keyframes shake {
